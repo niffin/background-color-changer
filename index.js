222241e6d20aa26c193fd78ee5d472b21955e8f1
Yotam Ginzburg <yotam@presencepg.com>
2017-07-07 19:04:25 -0700

    refactor to class in order to decrease runtime

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 import { ensureUnique, generateColor } from './utils';
 
-export default function changeBackground () {
-  const AMOUNT_OF_SECONDS = 10,
-        backgroundColors  = [];
+export default class ColorChanger {
+  constructor () {
+    this.AMOUNT_OF_SECONDS = 10;
+    this.backgroundColors  = [];
 
-  // create color array
-  for (let i = 0; i < AMOUNT_OF_SECONDS; i++) {
-    const newColor = ensureUnique(generateColor, backgroundColors);
-    backgroundColors.push(newColor);
+    this.changeBackground(1);
   }
 
-  // set up color changes
-  backgroundColors.forEach((color, index) => {
+  changeBackground (i) {
     setTimeout(() => {
+      const color = ensureUnique(generateColor, this.backgroundColors);
+      this.backgroundColors.push(color);
       document.body.style.backgroundColor = color;
-    }, (index + 1) * 1000);
-  });
+
+      if (i < this.AMOUNT_OF_SECONDS) {
+        this.changeBackground(i + 1);
+      }
+    }, 1000);
+  }
 }
